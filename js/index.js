54b8cad373a827c8490378e38940c977702e3f80
dzejkk <kontrajakub2@gmail.com>
2025-03-14 09:24:04 +0100

    added basic styles

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 const blogContainer = document.querySelector(".blogs");
 
+const renderPosts = async () => {
+  let uri = "http://localhost:8000/posts";
 
+  const response = await fetch(uri);
+  const posts = await response.json();
+
+  console.log(posts);
+
+  let template = "";
+
+  posts.map((post) => {
+    template += `
+                <div class="blog-post-container">
+                    <h1>${post.title}</h2>
+                    <p>${post.author}</p>
+                    <article>${post.content}</article>
+                </div>`   ;
+  });
+
+  blogContainer.innerHTML = template;
+};
+
+window.addEventListener("DOMContentLoaded", (e) => renderPosts());
