be22e14a94082c910fc8c971b315834080fa69b0
dzejkk <kontrajakub2@gmail.com>
2025-03-17 15:46:20 +0100

    added details page

---
+++ js/details.js
@@ -0,0 +1,28 @@
+const id = new URLSearchParams(window.location.search).get("id");
+const container = document.querySelector(".details");
+
+
+const renderDetails = async ()=> {
+
+    const res =  await fetch("http://localhost:8000/posts/" + id);
+    const post = await res.json();
+
+
+    const template = `
+      <h1>${post.title}</h1>
+      <small>${post.date}</small>
+      <p>${post.content}</p>
+
+    `
+    
+    container.innerHTML = template;
+
+
+
+
+
+}
+
+
+
+window.addEventListener("DOMContentLoaded", ()=> renderDetails())
--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ const renderPosts = async () => {
                     <h2>${post.title}</h2>
                     <p class="author">${post.author}</p>
                     <article>${post.content.slice(0,100)}</article>
-                    <a href=/details.html>Read more</a>
+                    <a href="/details.html?id=${post.id}">Read more</a>
                 </div>`   ;
   });
 
