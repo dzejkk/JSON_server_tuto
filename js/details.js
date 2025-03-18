79cc864f500c72f189ece8c865d8246aa14fea3c
dzejkk <kontrajakub2@gmail.com>
2025-03-18 16:36:36 +0100

    added delete button

--- js/details.js
+++ js/details.js
@@ -1,28 +1,28 @@
-const id = new URLSearchParams(window.location.search).get("id"); // to get id 
+const id = new URLSearchParams(window.location.search).get("id"); // to get id
 const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".button");
 
+const renderDetails = async () => {
+  const res = await fetch("http://localhost:8000/posts/" + id); // getting one item + id !
+  const post = await res.json();
 
-const renderDetails = async ()=> {
-
-    const res =  await fetch("http://localhost:8000/posts/" + id); // getting one item + id !
-    const post = await res.json();
-
-
-    const template = `
+  const template = `
       <h1>${post.title}</h1>
       <small>${post.date}</small>
       <p>${post.content}</p>
 
-    `
-    
-    container.innerHTML = template;
+    `;
+  container.innerHTML = template;
+};
 
+// Delete request
 
+deleteBtn.addEventListener("click", async (e) => {
+  const res = await fetch("http://localhost:8000/posts/" + id, {
+    method: "DELETE",
+  });
 
+  window.location.replace("/");
+});
 
-
-}
-
-
-
-window.addEventListener("DOMContentLoaded", (e)=> renderDetails())
+window.addEventListener("DOMContentLoaded", (e) => renderDetails());
